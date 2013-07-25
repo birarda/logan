de6384225842b4e6243d2204e020fbc6ffd74602
Stephen Birarda <commit@birarda.com>
2013-07-25 16:01:53 -0700

    document the HashConstructed class

--- lib/logan/HashConstructed.rb
+++ lib/logan/HashConstructed.rb
@@ -1,5 +1,10 @@
 module HashConstructed
- def initialize(h)
-  h.each { |k,v| send("#{k}=",v) if respond_to?("#{k}=") } 
- end
+  
+  # initalizes the object by pulling key-value pairs from the hash and 
+  # mapping them to the object's instance variables
+  # 
+  # @param [Hash] h hash containing key-value pairs to map to object instance variables
+  def initialize(h)
+    h.each { |k,v| send("#{k}=",v) if respond_to?("#{k}=") } 
+  end
 end
