148f24ce5e0a406dd917e2139cb76e6d3a006794
Stephen Birarda <commit@birarda.com>
2013-07-25 12:58:09 -0700

    give project ID to list in global todolists

--- lib/logan/client.rb
+++ lib/logan/client.rb
@@ -12,7 +12,21 @@ module Logan
     
     def projects
       response = self.class.get '/projects.json'
-      response.parsed_response.map { |p| p = Logan::Project.new(p) }
+      response.parsed_response.map { |h| p = Logan::Project.new(h) }
+    end
+    
+    def todolists
+      response = self.class.get '/todolists.json'
+      
+      response.parsed_response.map do |h| 
+        list = Logan::TodoList.new(h)
+        
+        # grab the project ID for this list from the url
+        list.project_id = list.url.scan( /projects\/(\d+)/).last.first
+        
+        # return the list so this method returns an array of lists
+        list
+      end
     end
   end
 end
