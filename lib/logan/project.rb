5594c679f80358fd2950f3f1e79623cfd4b47dba
Stephen Birarda <commit@birarda.com>
2013-07-25 16:11:19 -0700

    documentation for Project class

--- lib/logan/project.rb
+++ lib/logan/project.rb
@@ -8,6 +8,9 @@ module Logan
     attr_accessor :id
     attr_accessor :name
     
+    # get active todo lists for this project from Basecamp API
+    # 
+    # @return [Array<Logan::TodoList>] array of active todo lists for this project
     def todolists
       active_response = Logan::Client.get "/projects/#{@id}/todolists.json"
       lists_array = active_response.parsed_response.map do |h| 
@@ -15,6 +18,9 @@ module Logan
       end
     end
     
+    # get completed todo lists for this project from Basecamp API
+    # 
+    # @return [Array<Logan::TodoList>] array of completed todo lists for this project
     def completed_todolists
       completed_response = Logan::Client.get "/projects/#{@id}/todolists/completed.json"
       lists_array = completed_response.parsed_response.map do |h| 
@@ -22,18 +28,29 @@ module Logan
       end
     end
     
+    # get both active and completed todo lists for this project from Basecamp API
+    # 
+    # @return [Array<Logan::TodoList>] array of active and completed todo lists for this project
     def all_todolists
       todolists + completed_todolists
     end
     
+    # get an individual todo list for this project from Basecamp API
+    # 
+    # @param [String] list_id id for the todo list
+    # @return [Logan::TodoList] todo list instance
     def todolist(list_id)
       response = Logan::Client.get "/projects/#{@id}/todolists/#{list_id}.json"
       Logan::TodoList.new response.parsed_response.merge({ :project_id => @id })
     end
     
-    def create_todolist(todolist)
+    # create a todo list in this project via Basecamp API
+    # 
+    # @param [Logan::TodoList] todo_list todo list instance to be created
+    # @return [Logan::TodoList] todo list instance from Basecamp API response
+    def create_todolist(todo_list)
       post_params = {
-        :body => todolist.post_json,
+        :body => todo_list.post_json,
         :headers => Logan::Client.headers.merge({'Content-Type' => 'application/json'})
       }
           
