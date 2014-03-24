dcd40de8ab7935e57b5884c18d85c469cf1a999b
Shulhi Sapli <shulhi@gmail.com>
2014-03-24 13:19:24 +0800

    add people.json to client

--- lib/logan/client.rb
+++ lib/logan/client.rb
@@ -72,5 +72,9 @@ module Logan
       response = self.class.get "/events.json?since=#{since_time.to_s}&page=#{page}"
       response.map { |h| e = Logan::Event.new(h) }
     end
+
+    def people
+      response = self.class.get "/people.json"
+    end
   end
 end
