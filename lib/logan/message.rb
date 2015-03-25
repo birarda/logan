abcd44f84d63e4950b1c48e488ccc20fcabfde64
stevenbristol <stevenbristol@gmail.com>
2015-03-25 14:52:43 -0400

    forgot to add the files d'oh

---
+++ lib/logan/message.rb
@@ -0,0 +1,19 @@
+require 'logan/HashConstructed'
+
+module Logan
+  class Message
+    include HashConstructed
+
+    attr_accessor :id
+    attr_accessor :subject
+    attr_accessor :content
+    attr_accessor :private
+    attr_accessor :trashed
+    attr_accessor :subscribers
+    attr_accessor :creator
+    attr_accessor :comments
+
+
+
+  end
+end
